436eee073b7edaf8eacaae6ed3bbf8b5c0893acc
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-23 21:40:32 +0000

    added cors setup

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 import express from 'express';
 import { config } from 'dotenv';
 config();
+import cors from 'cors';
 
 import routes from './src/routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome! School Management System');
-});
+const allowedOrigins = new Set(
+  process.env.CORS_ACCESS ? process.env.CORS_ACCESS.split(',') : []
+);
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.has(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use('/', routes);
 
 const port = process.env.PORT || 3000;
-
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+  console.log('\x1b[34m%s\x1b[0m', ` http://localhost:${port}/`);
+  console.log(`Allowed origins: ${allowedOrigins}`);
 });
