6f83d6686d54dad1b403f190062033c093be7161
Nurudeen <abdulmajeednurudeen48@gmail.com>
2024-11-23 21:30:41 +0000

    added prisma to dev dependencies, created index routes setup and imported it to server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import { config } from 'dotenv';
+config();
+
+import routes from './src/routes';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome! School Management System');
 });
+
+app.use('/', routes);
 
 const port = process.env.PORT || 3000;
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,7 @@
 // src/routes/index.js
+import express from 'express';
+const routes = express.Router();
+
+// router.use('/', userRouter);
+
+export default routes;
